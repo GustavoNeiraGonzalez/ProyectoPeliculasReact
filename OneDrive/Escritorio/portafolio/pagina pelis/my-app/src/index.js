ed15eb112cfd590c9dc3aecc36a7a703b7069b1e
Gustavo <gustavoeduardoneiragonzalez@gmail.com>
2022-04-15 01:25:35 -0400

    comienzo react, ejemplos practicos

---
+++ OneDrive/Escritorio/portafolio/pagina pelis/my-app/src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+
+
+const divTituloParrafo = (/* ejemplo de entregar un div con datos,
+para llamarlo desde el reactDOM.render solo hace falta escribir divTituloParrafo en lugar 
+agregarles tambien <> etc */
+    <div>
+        <h1>Hola desde react</h1>
+        <p>texto parrafo desde react</p>
+    </div>
+);
+
+function Componente(props){
+    /* Las funciones deben empezar en mayusculas o tirará error*/
+    console.log(props); /*Props son las variables entregadas en el render, utilizamos console.log solo para 
+    visualizarlas en la consola del navegador */
+
+    const contenidoP = props.contenido;/*aqui solo creamos una constante para darla en el parrafo*/
+
+    return(/* utilizando props pasarás las variables donde las necesites */
+        <div>
+            <h1>{props.titulo}</h1> 
+            <p>{contenidoP}</p>
+        </div>
+    );
+}
+ReactDOM.render(<Componente titulo="Pelis:D" contenido="Hola pelis desde React"/>, document.getElementById("root"));

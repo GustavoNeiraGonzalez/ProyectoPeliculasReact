932e5ef7f9f9bdb0214fd60e13c8a4133c448586
Gustavo <gustavoeduardoneiragonzalez@gmail.com>
2022-04-15 22:30:22 -0400

    avanze componentes creados en archivos separados jsx

--- OneDrive/Escritorio/portafolio/pagina pelis/my-app/src/app.jsx
+++ OneDrive/Escritorio/portafolio/pagina pelis/my-app/src/app.jsx
@@ -1,1 +1,4 @@
-function 
+export function App(){ /* si no tiene export no podra ser uswado en el indexjs
+    y como en node los modulos son privados se requiere que el modulo/funcion/componente tenga exports */
+    return <div>App</div>;
+}

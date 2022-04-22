8ec2bfdf79a72baea5829a1ac9bb3dfadefb3274
Gustavo <gustavoeduardoneiragonzalez@gmail.com>
2022-04-22 00:57:07 -0400

    Creacion componente css y llamandolo de manera diferente en el componente moviecard.jsx

--- OneDrive/Escritorio/portafolio/pagina pelis/my-app/src/movieCard.jsx
+++ OneDrive/Escritorio/portafolio/pagina pelis/my-app/src/movieCard.jsx
@@ -1,4 +1,12 @@
+import styles from "./movieCard.module.css"//Moviendo el css al estilo modulo a traves de styles
+//para luego entregarlo en el classname, es decir que  utilizando 
+// styles.MovieCard estas llamando al estilo del class .movieCard creado en el css
 
 export function MovieCard({movie}){
-    return <li>{movie.title}</li>; //Se le coloca un key obligatorio para optimizar la lista (de esta forma no renderizas innecesariamente la lista si le haces modificaciones)
+    const imageUrl = "https://image.tmdb.org/t/p/w300" + movie.poster_path
+    //creando el url de las imagenes con el formato + la informacion de movies.json
+    return <li className={styles.movieCard}> 
+        <img src={imageUrl} alt={movie.title} />
+        <div>{movie.title}</div>
+        </li>; //Se le coloca un key obligatorio para optimizar la lista (de esta forma no renderizas innecesariamente la lista si le haces modificaciones)
 }

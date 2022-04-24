25772435008c142a64a3e7eb893cd4ff043b8542
Gustavo <gustavoeduardoneiragonzalez@gmail.com>
2022-04-23 20:10:08 -0400

    agregado titulo con estilo peli de terror

--- OneDrive/Escritorio/portafolio/pagina pelis/my-app/src/app.jsx
+++ OneDrive/Escritorio/portafolio/pagina pelis/my-app/src/app.jsx
@@ -1,10 +1,11 @@
 import { PelisGrid } from "./PelisGrid";
+import style from "./app.module.css";
 
 export function App(){ /* si no tiene export no podra ser uswado en el indexjs
     y como en node los modulos son privados se requiere que el modulo/funcion/componente tenga exports */
     return <div className="ContenedorTituloP">
         <header>
-            <h1>Peliculas :D</h1>
+            <h1 className={style.title}>Peliculas :D</h1>
         </header>
         <main>
             <PelisGrid></PelisGrid>

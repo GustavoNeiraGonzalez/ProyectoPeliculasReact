90c14dc67cb9471a617590fa7fd5e49e292bf97a
GustavoNeiraGonzalez <gustavoeduardoneiragonzalez@gmail.com>
2022-04-27 03:20:14 -0400

    mejora explicación de arrays, y mejoras en los detalles de moviedetails.jsx

--- pagina pelis/my-app/src/pages/movieDetails.jsx
+++ pagina pelis/my-app/src/pages/movieDetails.jsx
@@ -10,15 +10,18 @@ export function MovieDetails(){
         <img src={imageUrl} alt={movie.title} />
         <div>
             <p>
-                <strong>Title:</strong> {movie.title} 
+                <strong>Title: </strong> {movie.title} 
             </p>
             <p>
-                {movie.genres.map(genre => genre.name)}
+                <strong>Generos: </strong>
+                {movie.genres.map(genre => genre.name).join(", ")}
                 {/*Importante, aqui hay otro tipos de datos guardados, el arreglo
-                que contiene objetos ej; genres:[id:3, name:"asd"],[id:2, name:"terror"]
-                 un arreglo(arry) normal es de esta forma ej: array:["ola","quetal"]
-                 entonces para recorrer varias veces el genero, ya que queremos mostrar 
-                 los 2 generos que tiene la pelicula, utilizamos map*/}
+                que contiene objetos ej; genres:[{id:3, name:"asd"},{id:2, name:"terror"}]
+                 un arreglo(arry) normal es de esta forma ej: array:{saludo:"ola",pregunta:"quetal"}
+                 entonces para convertir el genero en string ya que queremos mostrar 
+                 los 2 generos que tiene la pelicula, utilizamos map y para que no esten
+                 pegados los generos ej: accionaventura, utilizamos join(", ") asi se 
+                 separaria por coma y espacio*/}
 
             </p>
             <p>

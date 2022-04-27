57763def66fb10050e3699b7848d92254fade08c
GustavoNeiraGonzalez <gustavoeduardoneiragonzalez@gmail.com>
2022-04-27 03:09:22 -0400

    agregado arreglo de objetos, mas explicacion en comentarios en moviedetails.jsx

--- pagina pelis/my-app/src/pages/movieDetails.jsx
+++ pagina pelis/my-app/src/pages/movieDetails.jsx
@@ -8,6 +8,22 @@ export function MovieDetails(){
     const imageUrl = "https://image.tmdb.org/t/p/w300" + movie.poster_path;
     return <div className={styles.detailsContainer}>
         <img src={imageUrl} alt={movie.title} />
-        assd
+        <div>
+            <p>
+                <strong>Title:</strong> {movie.title} 
+            </p>
+            <p>
+                {movie.genres.map(genre => genre.name)}
+                {/*Importante, aqui hay otro tipos de datos guardados, el arreglo
+                que contiene objetos ej; genres:[id:3, name:"asd"],[id:2, name:"terror"]
+                 un arreglo(arry) normal es de esta forma ej: array:["ola","quetal"]
+                 entonces para recorrer varias veces el genero, ya que queremos mostrar 
+                 los 2 generos que tiene la pelicula, utilizamos map*/}
+
+            </p>
+            <p>
+                <strong>Description:</strong> {movie.overview}
+            </p>
+        </div>
         </div>
 }

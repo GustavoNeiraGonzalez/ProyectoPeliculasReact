058dbe2f20d015ebceb19dad6c833b91fd104aed
GustavoNeiraGonzalez <gustavoeduardoneiragonzalez@gmail.com>
2022-05-15 02:19:42 -0400

    Cambios, agregados informacion traida desde api themoviedatabase, uso de useeffect para realizar fetch

--- pagina pelis/my-app/src/components/PelisGrid.jsx
+++ pagina pelis/my-app/src/components/PelisGrid.jsx
@@ -1,11 +1,31 @@
 import { MovieCard } from "./movieCard";
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import pelis from "./movies.json";
 import styles from "./PelisGrid.module.css"
 
 export function PelisGrid(){
-    console.log(pelis);
-    
+    //console.log(pelis);
+    //let movies = [];
+    const [movies, setMovies] = useState([]);
+
+    //const movies = moviesState[0];
+    //const setMovies = moviesState[1];
+  
+    /*para hacer una llamada asincrona en react se usaa useeffect*/
+    useEffect(()=>{
+        fetch("https://api.themoviedb.org/3/discover/movie", {
+            headers:{
+                Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2ODM0MGQ0ODlhMWU0ZWIzYzliMDQ0YTEyOWIyMjQxYiIsInN1YiI6IjYyNzMzYWM3N2NmZmRhNzMxNmM1ZGQ5MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.st8ZVrVOOCBDafo04RUOKIxXfy04XsHMB0DRVYAC0U0",
+                "Content-Type": "application/json;charset=utf-8",
+            },
+        }).then((result) => result.json()).then((data) =>{
+            setMovies(data.results)
+        })
+        /*el result es de tipo promesa, para convertir en objeto
+        el resultado usamos json */
+    }, [])/*Al setear movies hace que se cree un ciclo infinito que repetira 
+    el useeffect, para impedirlo al final agregamos un array vacio, [] 
+    que indicará que se ejecute solamente una vez al cargar la pagina*/
     return (
         <ul className={styles.movieGrid}>
             {pelis.map((movie) =>(
--- pagina pelis/my-app/src/pages/movieDetails.jsx
+++ pagina pelis/my-app/src/pages/movieDetails.jsx
@@ -1,18 +1,34 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+import {get} from "../utils/httpclient"
 import movie from "./movie.json";
 import styles from "./movieDetails.module.css";
+import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 
 export function MovieDetails(){
-    console.log(movie.poster_path)
-    console.log(movie.title);
+    const { movieId } = useParams()
+    const [movie,setMovie] = useState(null);
+    
+    console.log(movieId)
+    useEffect(() =>{
+        get("/movie/" + movieId).then((data) => {
+            setMovie(data);
+        })
+    }, [movieId])/*Al darle el movieId hara que al cambiar
+    el valor entonces se ejecute el useeffect  */
+
+    if (!movie){
+        return null
+    } /*para que no de error de que movie este vació, ya que agarrará
+    un valor en el use effect */
     const imageUrl = "https://image.tmdb.org/t/p/w300" + movie.poster_path;
+
     return <div className={styles.detailsContainer}>
         <img className={styles.column + " " + styles.movieImage} src={imageUrl} alt={movie.title} />
         <div className={`${styles.column} ${styles.movieDetails}`}>
             <p className={styles.padding2parrafos}>
                 <strong>Title: </strong> {movie.title} 
             </p>
-            <p className={styles.padding2parrafos}>
+            <p className={`${styles.padding2parrafos} ${styles.firstItem}`}>
                 <strong>Generos: </strong>
                 {movie.genres.map(genre => genre.name).join(", ")}
                 {/*Importante, aqui hay otro tipos de datos guardados, el arreglo

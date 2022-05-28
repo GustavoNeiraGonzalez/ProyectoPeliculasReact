c73ed3168f4ee8461f42091fb4573ca889a525ca
GustavoNeiraGonzalez <71986954+GustavoNeiraGonzalez@users.noreply.github.com>
2022-05-27 21:01:12 -0400

    Delete OneDrive/Escritorio/portafolio directory

--- OneDrive/Escritorio/portafolio/pagina pelis/my-app/src/pages/LandingPage.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react"
-import { PelisGrid } from "../components/PelisGrid"
-export function LandingPage(){
-    return <PelisGrid/>
-}
--- OneDrive/Escritorio/portafolio/pagina pelis/my-app/src/pages/movieDetails.jsx
+++
@@ -1,4 +0,0 @@
-import React from "react"
-export function MovieDetails(){
-    return <div>Details</div>
-}
